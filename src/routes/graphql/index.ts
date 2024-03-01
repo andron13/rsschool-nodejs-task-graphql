ae01e03e7c5df38cfdbe419450826b9421f1f97e
Andrej Podlubnyj <andrej_podlubnyj@epam.com>
2024-03-01 08:52:24 +0100

    feat: start with a task. first endpoint

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,97 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  graphql,
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+} from 'graphql';
+
+export const memberType: GraphQLObjectType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    discount: { type: new GraphQLNonNull(GraphQLFloat) },
+    postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
+  },
+});
+
+export const postType = new GraphQLObjectType({
+  name: 'Post',
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLString) },
+  },
+});
+
+export const profileType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    memberTypeId: { type: new GraphQLNonNull(GraphQLString) },
+  },
+});
+
+export const subscribersOnAuthorsType = new GraphQLObjectType({
+  name: 'SubscribersOnAuthors',
+  fields: {
+    subscriberId: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLString) },
+  },
+});
+
+export const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: { type: profileType },
+    posts: { type: new GraphQLNonNull(new GraphQLList(postType)) },
+    userSubscribedTo: {
+      type: new GraphQLNonNull(new GraphQLList(subscribersOnAuthorsType)),
+    },
+    subscribedToUser: {
+      type: new GraphQLNonNull(new GraphQLList(subscribersOnAuthorsType)),
+    },
+  },
+});
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
+  const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'RootQueryType',
+      fields: {
+        memberTypes: {
+          type: new GraphQLList(memberType),
+          resolve: () => prisma.memberType.findMany(),
+        },
+        posts: {
+          type: new GraphQLList(postType),
+          resolve: () => prisma.post.findMany(),
+        },
+        users: {
+          type: new GraphQLList(userType),
+          resolve: () => prisma.user.findMany(),
+        },
+        profiles: {
+          type: new GraphQLList(profileType),
+          resolve: () => prisma.profile.findMany(),
+        },
+      },
+    }),
+  });
   fastify.route({
     url: '/',
     method: 'POST',
@@ -12,8 +101,13 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      return {};
+    async handler(req, reply) {
+      const { query, variables } = req.body;
+      return await graphql({
+        schema,
+        source: query,
+        variableValues: variables,
+      });
     },
   });
 };
